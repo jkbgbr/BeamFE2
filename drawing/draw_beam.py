47effa0dde8c034ac4ce0d859007dbf52d0be591
Jakab Gábor <jkbgbr@gmail.com>
2017-05-01 17:40:19 +0200

    supports are drawn

--- drawing/draw_beam.py
+++ drawing/draw_beam.py
@@ -8,17 +8,32 @@ def draw_structure(structure, show=True, deformed=True):
 
     if _plotting_available:
         for beam in structure.beams:
-            plt.plot([p.x for p in beam.nodes], [p.y for p in beam.nodes], 'k-')  # beams
-            plt.plot([p.x for p in beam.nodes], [p.y for p in beam.nodes], 'r.', markersize=8)  # nodes
+            # line width is in pixel
+            # node size is in pixel
+            plt.plot([p.x for p in beam.nodes], [p.y for p in beam.nodes], 'b-', linewidth=2)  # beams
+            plt.scatter([p.x for p in beam.nodes], [p.y for p in beam.nodes], marker='s', color='r')  # nodes
 
-            # plot BCs (axis-style)
-            # plot loads
+        # plot supports
+        # 1/scale will be the length of the bars representing the restricted translational degrees of freedom
+        scale = 20
+        figsize = [plt.rcParams["figure.dpi"] * x for x in plt.rcParams["figure.figsize"]][0]  # width of the fig in pixels
+        for k, v in structure.supports.items():
+            _aktnode = [x for x in structure.nodes if x.ID == k][0]
+            for dof in v:
+                if dof == 'ux':  # horizonatal
+                    plt.plot([_aktnode.x, _aktnode.x + figsize / scale], [_aktnode.y, _aktnode.y], 'b-', linewidth=4, alpha=0.5)  # a horizontal line
+                if dof == 'uy':  # vertical
+                    plt.plot([_aktnode.x, _aktnode.x], [_aktnode.y, _aktnode.y - figsize / scale], 'b-', linewidth=4, alpha=0.5)  # a horizontal line
+                if dof == 'rotz':  # rotation about Z
+                    plt.plot([_aktnode.x, _aktnode.x], [_aktnode.y, _aktnode.y], 'k.', markersize=16, alpha=0.5)  # a point
 
-            # http://12000.org/my_notes/stiffness_matrix/stiffness_matrix_report.htm
 
 
-            if deformed:
-                pass
+
+        # plot loads
+
+        if deformed:
+            pass
 
             # mp = self.midpoint
             # ax = plt.gca()
