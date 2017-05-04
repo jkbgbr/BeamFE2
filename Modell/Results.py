0780820be887ad12376910e35eb320df6162d78a
Jakab Gabor <jkbgbr@gmail.com>
2017-05-04 09:51:05 +0200

    updated results different analyses get different result objects

--- Modell/Results.py
+++ Modell/Results.py
@@ -1,9 +1,39 @@
 # -*- coding: utf-8 -*-
 
+import math
+
 
 class AnalysisResult(object):
     def __init__(self, parent=None):
         self.parent = parent
-        self.linear_static = None
-        self.modal = None
-        self.buckling = None
+
+
+class LinearStaticResult(AnalysisResult):
+    def __init__(self, parent=None):
+        super(LinearStaticResult, self).__init__(parent=parent)
+        self.parent = parent
+        self.displacements = None
+
+
+class ModalResult(AnalysisResult):
+    def __init__(self, parent=None, circular_freq=None, modalshapes=None):
+        super(ModalResult, self).__init__(parent=parent)
+        self.parent = parent
+        self.circular_frequencies = circular_freq
+        self.displacements = modalshapes
+
+    @property
+    def frequencies(self):
+        return [x/(2*math.pi) for x in self.circular_frequencies]
+
+    @property
+    def periods(self):
+        return [1./x for x in self.frequencies]
+
+
+class BucklingResult(AnalysisResult):
+    def __init__(self, parent=None, criticals=None, bucklingshapes=None):
+        super(BucklingResult, self).__init__(parent=parent)
+        self.parent = parent
+        self.criticals = criticals
+        self.displacements = bucklingshapes
