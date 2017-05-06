ff78a6c6520bd0a8cfee77b089ddea0f6426f3bc
Jakab Gábor <jkbgbr@gmail.com>
2017-05-06 11:56:03 +0200

    todo update

--- Modell/__init__.py
+++ Modell/__init__.py
@@ -17,10 +17,7 @@ np.set_printoptions(precision=6, suppress=True, linewidth=250)
 #   lumped mass matrix?
 #   modal masses, modal participation
 #   EC-based stuff: modal antwort, summation...
-#   ability to add mass
 #   ability to convert loads to masses - through defining gravity and its direction
 #   LATER: ability to choose which directions mass act?
 # Buckling:
 #   implement. the geometrical stiffness matrix is needed for that.
-# Display:
-#   displacements seem to be correct but plotting sucks
