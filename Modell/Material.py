f3ef30c84bf51da858e577506e7021c83f9e8bed
Jakab Gábor <jkbgbr@gmail.com>
2017-05-10 18:45:06 +0200

    updated materials and tests for it

--- Modell/Material.py
+++ Modell/Material.py
@@ -11,63 +11,54 @@ class Material(object):
         self.name = name
 
 
-class CustomLinearElasticMaterial(Material):
+class LinearElasticMaterial(Material):
+    # abstract class
     def __init__(self, name='custom', E=None, nu=None, rho=None):
         assert E is not None
         assert 0 < E
         assert nu is not None
         assert 0 < nu
         assert nu < 0.5
-        assert rho > 0
-        super(CustomLinearElasticMaterial, self).__init__(name=name)
-        self.E = E  # the elastic modulus
-        self.nu = nu  # Poisson's ration, dimensionless
-        self.rho = rho  # Density
-
-    def G(self):
-        return self.E/(2 * 1 + self.nu)
-
-
-class LinearElasticMaterial(Material):
-    # abstract class
-    def __init__(self, name=None):
+        assert rho >= 0
         super(LinearElasticMaterial, self).__init__(name=name)
-        self.E = None  # the elastic modulus in N/m2
-        self.nu = None  # Poisson's ration, dimensionless
-        self.rho = None
+        self._E = E  # the elastic modulus in N/m2
+        self._nu = nu  # Poisson's ration, dimensionless
+        self._rho = rho
 
+    def __repr__(self):
+        return "LinearElasticMaterial(name='%s', E=%r, nu=%r, rho=%r)" % (self.name, self.E, self.nu, self.rho)
+
+    @property
+    def E(self):
+        return self._E
+
+    @property
+    def nu(self):
+        return self._nu
+
+    @property
+    def rho(self):
+        return self._rho
+
+    @property
     def G(self):
         return self.E/(2 * 1 + self.nu)
 
+    def set_weightless(self):
+        self._rho = 0
+        if 'weightless' not in self.name:
+            self.name = ' '.join(['weightless', self.name])
 
-class RigidLinearElasticMaterial(LinearElasticMaterial):
-    def __init__(self, name='rigid'):
-        super(RigidLinearElasticMaterial, self).__init__(name=name)
-        self.E = 1e20  # the elastic modulus in N/m2
-        self.nu = 0.5  # Poisson's ration, dimensionless
-
-    def G(self):
-        return 1e20
+    def set_rigid(self):
+        self._E = 1e20
+        if 'rigid' not in self.name:
+            self.name = ' '.join(['rigid', self.name])
 
 
 class Steel(LinearElasticMaterial):
     # normal ideal elastic steel
-    def __init__(self, E=2.1e5, nu=0.3, name='steel'):
-        super(Steel, self).__init__(name=name)
-        self.rho = 7.850e-9  # to result correct modal results together with N, mm, second
-        self.E = E  # N/m2
-        self.nu = nu
+    def __init__(self, name='steel'):
+        super(Steel, self).__init__(E=2.1e5, nu=0.3, name=name, rho=7.85e-9)
 
-
-class WeightlessSteel(Steel):
-    # steel without weight
-    def __init__(self, name='weightless steel'):
-        super(WeightlessSteel, self).__init__(name=name)
-        self.rho = 0  # to result correct modal results together with N, mm, second
-
-
-class RigidSteel(Steel):
-    # steel considered infinitely rigid
-    def __init__(self, name='rigid steel'):
-        super(RigidSteel, self).__init__(name=name)
-        self.rho = 7.85e-9  # to result correct modal results together with N, mm, second
+    def __repr__(self):
+        return "Steel(E=%r, nu=%r, rho=%r)" % (self.E, self.nu, self.rho)
---
+++ Tests/test_material.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+from Modell.Material import *
+
+
+class Material_Test(unittest.TestCase):
+    """
+    basic tests for the materials. ONLY the engineering units are tested.
+    """
+
+    def test_LinearElasticMaterial(self):
+        # tests the assertions
+        args = []
+        kwargslist = []
+        kwargslist.append({'name': 'hooke', 'E': 25, 'nu': 0.4, 'rho': -0.1})  # negative density
+        kwargslist.append({'name': 'hooke', 'E': 0, 'nu': 0.4, 'rho': -0.1})  # E-module zero
+        kwargslist.append({'name': 'hooke', 'E': 25, 'nu': 0.6, 'rho': 0.1})  # Poisson to large
+        kwargslist.append({'name': 'hooke', 'E': 25, 'nu': 0, 'rho': 0.1})  # Poisson to small
+        for kw in kwargslist:
+            self.assertRaises(AssertionError, LinearElasticMaterial, *args, **kw)
+
+    def test_Steel(self):
+        steel = Steel()
+        self.assertTrue(steel.E == 2.1e5)
+        self.assertAlmostEqual(steel.G, 9.13e4, delta=10)
+        self.assertTrue(steel.nu == 0.3)
+        self.assertTrue(steel.rho == 7.85e-9)
+        self.assertTrue(steel.name == 'steel')
+        self.assertFalse(steel.E == 0)
+        self.assertTrue(repr(steel) == 'Steel(E=210000.0, nu=0.3, rho=7.85e-09)')
+
+    def test_set_rigid(self):
+        steel = Steel()
+        steel.set_rigid()
+        self.assertFalse(steel.E == 2.1e5)
+        self.assertTrue(steel.E == 1e20)
+        self.assertTrue('rigid' in steel.name)
+        steel.set_rigid()
+        self.assertTrue(steel.name.count('rigid') == 1)
+
+    def test_set_weightless(self):
+        steel = Steel()
+        steel.set_weightless()
+        self.assertFalse(steel.rho == 7.85e-9)
+        self.assertTrue(steel.rho == 0)
+        self.assertTrue('weightless' in steel.name)
+        steel.set_weightless()
+        self.assertTrue(steel.name.count('weightless') == 1)
+
