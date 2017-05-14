66339f35abcbcf8c4a31fad914e6a17b6371dc5c
Jakab Gábor <jkbgbr@gmail.com>
2017-05-14 11:26:55 +0200

    updated todo

--- Modell/__init__.py
+++ Modell/__init__.py
@@ -18,13 +18,13 @@ Time: [s]
 # todo: currently
 # todo: enable drawing the structure without having an analysis performed previously
 # todo: BCs -> support objects
+# todo: re-think plotting/drawing
 
 # use logging?
 # Tests:
 #   linear and modal analysis, units should be checked. 100 % test coverage?
 # Lin stat:
-#   finish and fix global reaction force calculation
-#   internal actions in internal points, also plot them
+#   add internal concentrated force, moment
 #   calculating stresses (Sections need stress points for this...)
 # Modal:
 #   check added mass etc. Chopra example with higher resolution
@@ -34,26 +34,16 @@ Time: [s]
 # Buckling:
 #   implement.
 # draw:
-#   beautify: support size should be scaled to overall size, colors changed.
-#   draw structure with loads only
+#   beautify: scaling all drawn elements.
+#   draw with subplots for lin stat
 #   draw global reaction forces
-#   draw moment loads, reactions
+#   draw moment loads
 
 
 # todo: LATER
 # Lin stat:
-#   adding loads off-node (concentrated force and moment, bending)
-#   http://12000.org/my_notes/stiffness_matrix/stiffness_matrix_report.htm for internal actions, at the end of the page
 #   LATER: defining hinges?
 #   LATER: 3D Beam element?
 # Modal:
 #   ability to convert loads to masses - through defining gravity and its direction
 #   LATER: ability to choose which directions mass act?
-
-#
-# off-node loads:
-# - solve the element to get nodal reactions -> apply nodal reactions as loads
-# - handle cases like concentrated force or moment and distributed load perpendicular to the beam axis
-# - distributed load: linearly changing, acting on arbitrary part of the beam as general case
-#
-#
