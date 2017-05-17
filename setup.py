665ece78f0d89cddff7c6ca7d7a6afebe1616104
Jakab Gabor <jkbgbr@gmail.com>
2017-05-17 07:26:05 +0200

    setup update

--- setup.py
+++ setup.py
@@ -8,9 +8,9 @@ from setuptools import setup
 
 setup(name='beamFE',
       version='0.1',
-      description='2D Beam finite element suite for linear static and modal analysis',
+      description='2D Beam finite element package for linear static and modal analysis',
       url='http://github.com/jkbgbr/beamFE2',
-      author='jake77',
+      author='Jakab Gabor',
       author_email='jkbgbr@gmail.com',
       license='GNU GPLv3',
       packages=['BeamFE2'],
